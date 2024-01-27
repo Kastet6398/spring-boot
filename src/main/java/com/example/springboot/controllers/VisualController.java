47da7886bcf3d5b585dd43f8198c273c3388cdde
Kastet6398 <konstantin.nezhalsky@gmail.com>
2024-01-27 13:47:42 +0200

    9

--- src/main/java/com/example/springboot/controllers/VisualController.java
+++ src/main/java/com/example/springboot/controllers/VisualController.java
@@ -2,22 +2,16 @@ package com.example.springboot.controllers;
 
 import com.example.springboot.models.BaseModel;
 import com.example.springboot.models.MessageModel;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api")
-public class ApiController {
-    private int a;
-    @PostMapping("/add")
-    public BaseModel add() {
-        a++;
-        return new MessageModel("added");
-    }
-    @GetMapping("/read")
-    public int read() {
-        return a;
+@Controller
+public class VisualController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
     }
 }
